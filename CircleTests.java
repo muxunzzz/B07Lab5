2511538fa8f98210e825b4b95ab46119de6664b4
muxun <muxun@Muxun-MacBook-Pro.local>
2024-10-22 13:04:08 -0400

    test: added JUnit test cases for Circle

---
+++ CircleTests.java
@@ -0,0 +1,41 @@
+package B07Lab5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import B07Lab5.Point;
+import B07Lab5.Circle;
+
+class CircleTests {
+
+	@Test
+	void test1() {
+		Point p = new Point(0,0);
+		Circle c = new Circle(p,1);
+		assertTrue(c.perimeter().equals(2 * Math.PI));
+	}
+
+	@Test
+	void test2() {
+		Point p = new Point(0,0);
+		Circle c = new Circle(p,1);
+		assertTrue(c.area().equals(Math.PI));
+	}
+
+	@Test
+	void test3() {
+		Point p1 = new Point(0,0);
+		Point p2 = new Point(0.5,0);
+		Circle c = new Circle(p1,1);
+		assertTrue(c.contains(p2));
+	}
+
+	@Test
+	void test4() {
+		Point p1 = new Point(0,0);
+		Point p2 = new Point(2,0);
+		Circle c = new Circle(p1,1);
+		assertFalse(c.contains(p2));
+	}
+}
